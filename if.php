afdf31a4178e0160014c20439963daa09d684ddc
nencyprajapati <137034946+nencyprajapati@users.noreply.github.com>
2023-07-03 11:28:08 +0530

    Add files via upload

---
+++ if.php
@@ -0,0 +1,24 @@
+<?php
+$x;
+$y;
+$z;
+if($x<$y){
+    echo" x is less than y"; 
+}
+else{
+    echo " x is not less than y";
+}
+if($y<$z){
+    echo " y is less than z";
+}
+else{
+    echo "y is not less than z"."\n";
+    ;
+}
+if ($x<$z){
+    echo "z is less than x"."\n";
+}
+else{
+    echo "z is not less than x"."\n";
+}
+?>
