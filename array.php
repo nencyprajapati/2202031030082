9501e1cc98b96efd2a9a34dc2a91140ec4c9b4f2
nencyprajapati <137034946+nencyprajapati@users.noreply.github.com>
2023-07-04 15:38:13 +0530

    Add files via upload

---
+++ array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$cars = array("Volvo", "BMW", "Toyota"); 
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+echo "Peter is " . $age['Peter'] . " years old.";
+?>
+
+</body>
+</html>
+
+
